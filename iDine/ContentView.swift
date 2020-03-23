1d2aa7103718f59e6128549bb3b2d8a05591c64e
Jeffrey Yang <yangjeffrey14@gmail.com>
2020-03-23 14:54:49 -0400

    commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Jeffrey Yang on 3/22/20.
+//  Copyright © 2020 JeffreyYang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
+    var body: some View {
+        NavigationView{
+            List {
+                ForEach(menu) { section in
+                    Section(header: Text(section.name)) {
+                        ForEach(section.items) { item in
+                            Text(item.name)
+                        }
+                    }
+                }
+            }.navigationBarTitle("Menu")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
